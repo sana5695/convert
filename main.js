83758d92f29c6dc274c5b93868e8e8a992c54fe2
sana5695 <deepbeatx@gmail.com>
2022-12-19 04:41:36 +0500

    done

--- main.js
+++ main.js
@@ -1,81 +1,65 @@
+const main = document.querySelector(".main")
+var div = document.createElement("div");
 
-div = document.createElement("div");
+const btn = document.querySelector('#btn')
 
-document.body.append(div);
+btn.addEventListener("click", () => {
 
+    const input = document.querySelector("#input").value
 
-async function getCurrencise(firstValute,secondValute, inputNumber){
+    a(input)
+
+})
+
+async function getCurrencise(firstValute, secondValute, inputNumber) {
+
     const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
     const data = await response.json()
     const resultJSON = await data;
-    
+
+    const RUB = {
+        CharCode: "RUB",
+        Name: "Рублей",
+        Nominal: 1,
+        Value: 1
+    }
+
+    resultJSON.Valute.RUB = RUB
+
     let result
     let resultString
 
-    if((firstValute!=="RUB" && resultJSON.Valute[firstValute]== undefined)||(secondValute !=="RUB" && resultJSON.Valute[secondValute]== undefined)){
+    if ((resultJSON.Valute[firstValute] == undefined) || (resultJSON.Valute[secondValute] == undefined)) {
         throw "Некоректная форма записи валюты";
     }
 
-    if (firstValute == "RUB" && secondValute == "RUB") {
-        result = inputNumber
-        resultString = "Рублей"
-    } else if (firstValute == "RUB"){
-        result = inputNumber / resultJSON.Valute[secondValute].Value
-        resultString = resultJSON.Valute[secondValute].Name
-    } else if (secondValute == "RUB") {
-        result = inputNumber * resultJSON.Valute[firstValute].Value
-        resultString = "Рублей"
-    } else {
-        result = (inputNumber / resultJSON.Valute[firstValute].Value)*resultJSON.Valute[secondValute].Value
-        resultString = resultJSON.Valute[secondValute].Name
-    }
+    result = (inputNumber / resultJSON.Valute[secondValute].Value) / resultJSON.Valute[firstValute].Nominal * resultJSON.Valute[firstValute].Value * resultJSON.Valute[secondValute].Nominal
+    resultString = resultJSON.Valute[secondValute].Name
 
-    result = (Math.floor(result * 100) / 100)
-    
-    
+    result = (Math.round(result * 100) / 100)
 
-    
-    div.innerHTML = `${result} ${resultString}` ;
-    
+    main.append(div);
 
+    div.innerHTML = `${result} ${resultString}`;
 
 }
 
-const btn = document.querySelector('#btn')
-btn.addEventListener("click", () => {
-    console.log("click")
-    const input = document.querySelector("#input").value
-    console.log(input)
-    a(input)
-    
-})
-
-
 function a(input) {
-    console.log(input)
 
     inputNumber = Number(input.replace(/\D/g, ''))
-
     inputString = input.replace(/\d/g, '').replace(/\s/g, '').toUpperCase()
-    
-    
 
-    if (~input.indexOf(inputNumber)) {
-
-    } else {
+    if (~input.indexOf(inputNumber)) {} else {
         throw "Некоректная форма записи числа";
     }
 
-    if (inputString.length !== 8) {
+    if ((inputString.length !== 8) || (inputString.slice(3, 5) !== "IN")) {
         throw "Некоректная форма записи преобразования";
     }
 
-    const firstValute = inputString.slice(0,3)
-    const secondValute = inputString.slice(5,9)
+    const firstValute = inputString.slice(0, 3)
+    const secondValute = inputString.slice(5, 9)
 
-    
-    
+    getCurrencise(firstValute, secondValute, inputNumber)
 
-    
 }
-
--- script.js
+++ script.js
@@ -1,28 +1,68 @@
-
-
-
 getCurrencise()
 
+async function getCurrencise() {
 
-async function getCurrencise(){
+    const responseGeo = await fetch('http://www.geoplugin.net/json.gp?ip=xx.xx.xx.xx')
+    const geo = await responseGeo.json()
+    const geoJSON = await geo
+
     const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
     const data = await response.json()
     const resultJSON = await data;
-    
-    for (var key in resultJSON.Valute) {
-        
-        var element = resultJSON.Valute[key];
-        var number=element.Value
-        number = (Math.floor(number * 100) / 100)
-        console.log(element.CharCode);
-        div = document.createElement("div");
-        document.body.append(div);
-        div.innerHTML = `${element.CharCode} ${number}`;
-        
-      };
 
+    const RUB = {
+        CharCode: "RUB",
+        Name: "Рублей",
+        Nominal: 1,
+        Value: 1
+    }
 
+    resultJSON.Valute.RUB = RUB
+    console.log(resultJSON)
 
+    var location = geoJSON.geoplugin_currencyCode;
 
+    const select = document.querySelector(".select")
+    const main = document.querySelector('.main')
+
+    chosed(location)
+
+    location = select.addEventListener("click", () => {
+        select.addEventListener('change', function () {
+            location = this.value
+            main.innerHTML = ""
+            chosed(location)
+        });
+    })
+
+    function chosed(location) {
+
+        select.innerHTML = ""
+
+        for (var key in resultJSON.Valute) {
+
+            var element = resultJSON.Valute[key];
+            var number = element.Value
+            var option = document.createElement("option")
+
+            select.append(option)
+            option.innerHTML = `${element.CharCode}`
+
+            if (location == element.CharCode) {
+                option.setAttribute("selected", "selected")
+            } else {
+                select.append(option)
+                option.innerHTML = `${element.CharCode}`
+
+                number = 1 / resultJSON.Valute[location].Value / element.Nominal * number * resultJSON.Valute[location].Nominal
+
+                number = (Math.round(number * 100) / 100);
+
+                var div = document.createElement("div");
+                div.classList.add('cell');
+                main.append(div)
+                div.innerHTML = `1 <span>${element.CharCode}</span> = ${number} <span>${location}</span>`;
+            }
+        };
+    }
 }
-
